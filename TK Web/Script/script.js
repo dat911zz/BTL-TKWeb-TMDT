823435374e6819b17e1d5ded47f3e19402bbd445
Vũ Đạt <datcy2011@gmail.com>
2022-06-03 14:58:25 +0700

    fixed tag

--- TK Web/Script/script.js
+++ TK Web/Script/script.js
@@ -1,3 +1,24 @@
 var path = window.location.pathname; //Get current location
 var page = path. split("/"). pop(); //Get page name
-console. log(page);
+console.log(page);
+
+var menus = document.querySelectorAll(".nav-link");
+
+menus.forEach( (menu, index) => {
+    console.log(menu, index);
+
+    menu.addEventListener("click", () => {
+        document.querySelector(".nav-link.active").classList.remove("active");
+        console.log(menu, index);
+        console.log("SOS");
+        menu.classList.add("active");
+    })
+
+
+    console.log(menu.id);
+    if(page == menu.id)
+    {
+        document.querySelector(".nav-link.active").classList.remove("active");
+        menu.classList.add("active");
+    }
+})
