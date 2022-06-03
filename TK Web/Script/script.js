d1fa4574e62e5d30adfd243a50f0449369410bf1
Vũ Đạt <datcy2011@gmail.com>
2022-06-03 15:39:03 +0700

    update footer 1.0

--- TK Web/Script/script.js
+++ TK Web/Script/script.js
@@ -1,24 +1,23 @@
 var path = window.location.pathname; //Get current location
-var page = path. split("/"). pop(); //Get page name
+var page = path.split("/").pop(); //Get page name
 console.log(page);
 
 var menus = document.querySelectorAll(".nav-link");
 
-menus.forEach( (menu, index) => {
+menus.forEach((menu, index) => {
     console.log(menu, index);
 
-    menu.addEventListener("click", () => {
-        document.querySelector(".nav-link.active").classList.remove("active");
-        console.log(menu, index);
-        console.log("SOS");
-        menu.classList.add("active");
-    })
+    // menu.addEventListener("click", () => {
+    //     document.querySelector(".nav-link.active").classList.remove("active");
+    //     console.log(menu, index);
+    //     console.log("SOS");
+    //     menu.classList.add("active");
+    // })
 
-
-    console.log(menu.id);
-    if(page == menu.id)
-    {
+    if (menu.id == page) {
         document.querySelector(".nav-link.active").classList.remove("active");
         menu.classList.add("active");
     }
+
+    console.log(menu.id);
 })
