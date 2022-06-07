72842394980ef19313c81e141984514c681b9ad4
Vũ Đạt <datcy2011@gmail.com>
2022-06-07 23:25:14 +0700

    update index.html

--- Script/script.js
+++ Script/script.js
@@ -21,3 +21,19 @@ menus.forEach((menu, index) => {
 
     console.log(menu.id);
 })
+
+
+
+  $('.multi-item-carousel .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    if (next.next().length>0) {
+      next.next().children(':first-child').clone().appendTo($(this));
+    } else {
+      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+  });
