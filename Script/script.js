c0e46e65ea84cd128169fa78ccd4158f78d38ed0
Vũ Đạt <datcy2011@gmail.com>
2022-06-09 22:28:53 +0700

    update countdown

--- Script/script.js
+++ Script/script.js
@@ -6,14 +6,6 @@ var menus = document.querySelectorAll(".nav-link");
 
 menus.forEach((menu, index) => {
     console.log(menu, index);
-
-    // menu.addEventListener("click", () => {
-    //     document.querySelector(".nav-link.active").classList.remove("active");
-    //     console.log(menu, index);
-    //     console.log("SOS");
-    //     menu.classList.add("active");
-    // })
-
     if (menu.id == page) {
         document.querySelector(".nav-link.active").classList.remove("active");
         menu.classList.add("active");
@@ -21,7 +13,6 @@ menus.forEach((menu, index) => {
 
     console.log(menu.id);
 })
-
 
 
   $('.multi-item-carousel .item').each(function(){
@@ -38,5 +29,38 @@ menus.forEach((menu, index) => {
     }
   });
 
+  // Đếm ngược thời gian refresh hàng hot
+  var fullTime = new Date("july 19, 2022 16:40:00").getTime();
+        setInterval(() => {
+            var now = new Date().getTime();
+            // now = new Date("may 31, 2022 16:40:00").getTime();
+            var duration = fullTime - now;
+
+            var days = Math.floor(duration/(1000*60*60*24));
+            var hours = Math.floor(duration/(1000*60*60));
+            var mins = Math.floor(duration/(1000*60));
+            var secs = Math.floor(duration/(1000));
+
+            hours %= 24;
+            mins %= 60;
+            secs %= 60;
+
+            document.getElementById("dd").innerHTML = days;
+            document.getElementById("hh").innerHTML = hours;
+            document.getElementById("mm").innerHTML = mins;
+            document.getElementById("ss").innerHTML = secs;
+
+            if (duration <= 0){
+
+                document.getElementById("dd").innerHTML = 0;
+                document.getElementById("hh").innerHTML = 0;
+                document.getElementById("mm").innerHTML = 0;
+                document.getElementById("ss").innerHTML = 0;
+                var end = document.querySelector(".end");
+                end.style.display = "block";
+                end.style.color = "red";
+                end.style.textAlign = "center";
+            }
+        }, 1000)
 
 
